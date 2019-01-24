0c8dcffa2984a157f0fe51746a4191bf0b1fd262
mattlaura <lhuiefiction@gmail.com>
2019-01-24 16:49:21 -0500

    Initial commit

---
+++ Lab10/Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Circle
+    {
+        private double radius;
+        public static int circleCount=0;
+        public Circle (double radius)
+        {
+            this.radius = radius;  
+            circleCount++;
+        }
+
+        public double CalculateCircumference()
+        { 
+            double circumference = Math.PI * 2 * radius;
+            return circumference;
+        }
+
+        public string CalcualteFormattedCircumference()
+        {
+           double formatedCircumference= Math.Round(CalculateCircumference(),2);
+            string circumferenceString = FormatDouble(formatedCircumference);
+            return circumferenceString;
+        }
+
+        public double CalculatedArea()
+        { 
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+
+        public string CalculatedFormattedArea()
+        {
+            double formatedArea = Math.Round(CalculatedArea(), 2);
+            string areaString = FormatDouble(formatedArea);
+            return areaString;
+        }
+
+        public string FormatDouble(double x)
+        {
+            string FormatDouble = x.ToString();
+            return FormatDouble;
+        }
+
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> circles = new List<int>();
+            double radius = 0.0;
+            bool run = true;
+            while (run)
+            {
+                bool runRadius = true;
+                while (runRadius)
+                {
+
+                    Console.WriteLine("Please enter a radius:");
+                    
+                    try
+                    {
+                        radius = double.Parse(Console.ReadLine());
+                        runRadius = false;
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine("I did not understand. Enter a number instead.");
+                        runRadius = true;
+                    }
+                }
+
+                Circle c = new Circle(radius);
+
+                Console.WriteLine("The circumference of your circle is " + c.CalcualteFormattedCircumference());
+                Console.WriteLine("The area of your circle is " + c.CalculatedFormattedArea());
+
+                run = Continue();
+
+            }
+            Console.WriteLine("You created {0} circle objects.", Circle.circleCount);
+        }
+        public static bool Continue()
+        {
+            bool run;
+            Console.WriteLine("Would you like to Continue (y/n): ");
+            string userAnswer = Console.ReadLine();
+            if (userAnswer == "y")
+            {
+                run = true;
+            }
+            else if (userAnswer == "n")
+            {
+                run = false;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, I did not understand, please try again");
+                return Continue();
+            }
+            return run;
+        }
+    }
+
+}
